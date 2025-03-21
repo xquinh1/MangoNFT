28920e95e388e29043a91cc4d673bdf1d71df00a
xquinh1 <nguyenxuanquynh.2032001@gmail.com>
2025-03-22 02:11:56 +0700

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript ví dụ (Nếu bạn muốn thêm tính năng tương tác trong tương lai)
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Trang web đã tải thành công!");
+});
