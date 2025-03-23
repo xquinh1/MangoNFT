8dc684b0905787d87ad30c549a68e9cff265b43c
xquinh1 <nguyenxuanquynh.2032001@gmail.com>
2025-03-23 18:57:02 +0700

    .

--- script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript ví dụ (Nếu bạn muốn thêm tính năng tương tác trong tương lai)
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Trang web đã tải thành công!");
-});
